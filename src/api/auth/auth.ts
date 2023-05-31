1a0658c671385f011ab3251295d4c69f0b016210
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2023-05-31 09:29:51 +0800

    Create AuthAPI class to handle authentication requests

---
+++ src/api/auth/auth.ts
@@ -0,0 +1,26 @@
+import { Axios } from "axios";
+import { SignInValues, SignUpValues } from "../../types/Auth/auth";
+
+export default class AuthAPI {
+    private authUrl = (endpoint: string) => `auth${endpoint}`;
+
+    constructor(private axiosClient: Axios) {}
+
+    async signIn(values: SignInValues) {
+        const response = await this.axiosClient.post(
+            this.authUrl("/sign-in"),
+            JSON.stringify(values)
+        );
+
+        console.log(response);
+    }
+
+    async signUp(values: SignUpValues) {
+        const response = await this.axiosClient.post(
+            this.authUrl("/sign-up"),
+            JSON.stringify(values)
+        );
+
+        console.log(response);
+    }
+}
