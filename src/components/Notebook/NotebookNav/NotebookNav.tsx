49b1dee30540acd35547a20138f47af47f1ffd41
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2023-06-01 12:49:15 +0800

    Minor changes

--- src/components/Notebook/NotebookNav/NotebookNav.tsx
+++ src/components/Notebook/NotebookNav/NotebookNav.tsx
@@ -3,22 +3,28 @@ import scss from "./notebookNav.module.scss";
 import { useAuth } from "../../../contexts/Auth";
 import { AppTitle } from "../../Common";
 import { HiArrowCircleRight, HiOutlineBookOpen, HiPlus } from "react-icons/hi";
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 
 const NotebookNav = () => {
     const { signOutAction } = useAuth();
+    const navigate = useNavigate();
 
     return (
         <nav className={scss["notebook-nav-container"]}>
             <div className={scss["notebook-nav-header"]}>
-                <AppTitle />
+                <span onClick={() => navigate("/")}>
+                    <AppTitle />
+                </span>
             </div>
 
             <div className={scss["notebook-nav-actions"]}>
                 <NavLink to={"/"} className={scss["notebook-nav-btn"]}>
                     <HiPlus />
                 </NavLink>
-                <NavLink to={"/notebook-list"} className={scss["notebook-nav-btn"]}>
+                <NavLink
+                    to={"/notebook-list"}
+                    className={scss["notebook-nav-btn"]}
+                >
                     <HiOutlineBookOpen />
                 </NavLink>
                 <button
